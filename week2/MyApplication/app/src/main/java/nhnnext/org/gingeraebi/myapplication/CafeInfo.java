77d471244000aa2d291f8b2de95569d56d9bfd24
JaeBongLee <ljb0484@gmail.com>
2016-02-24 16:32:50 +0900

    week1 week2 Folder

---
+++ week2/MyApplication/app/src/main/java/nhnnext/org/gingeraebi/myapplication/CafeInfo.java
@@ -0,0 +1,19 @@
+package nhnnext.org.gingeraebi.myapplication;
+
+/**
+ * Created by JaeWon on 2016-02-20.
+ */
+public class CafeInfo {
+    private String logoImageName;
+    private String cafeName;
+
+    public CafeInfo(String cafeName) {
+        this.cafeName = cafeName;
+
+    }
+
+    public String getCafeName() {
+        return cafeName;
+    }
+
+}
